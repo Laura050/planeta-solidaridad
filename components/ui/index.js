405fccfd66010832e304ba96d3d7f942717cd7a7
Laura050 <nizbol72@gmail.com>
2025-02-09 15:25:11 +0100

    Create index.js

---
+++ components/ui/index.js
@@ -0,0 +1,4 @@
+export { Button } from './button'
+export { Card, CardHeader, CardContent } from './card'
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs'
+export { Alert, AlertDescription } from './alert'
